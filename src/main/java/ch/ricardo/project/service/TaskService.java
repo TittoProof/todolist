156272799b12dd536649f798dc026dc6b6ac7bfe
tfortin <tfortin@vulog.com>
2018-02-21 17:29:50 +0100

    task service template

--- src/main/java/ch/ricardo/project/service/TaskService.java
+++ src/main/java/ch/ricardo/project/service/TaskService.java
@@ -1,8 +1,12 @@
 package ch.ricardo.project.service;
 
 import ch.ricardo.project.entity.Task;
+import ch.ricardo.project.repository.TaskRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,8 +17,27 @@ import org.springframework.stereotype.Service;
 public class TaskService {
     private final static Logger logger = LoggerFactory.getLogger(TaskService.class);
     
+    @Autowired
+    private TaskRepository taskRepository;
+    
     
     public Task createTask() {
+        return new Task();
+    }
+    
+    public List<Task> getAllTask() {
+        return new ArrayList();
+    }
+    
+    public Task getTask(int id) {
+        return new Task();
+    }
+    
+    public void deleteTask(int id) {
+        
+    }
+    
+    public Task updateTask(Task task) {
         return new Task();
     }
     

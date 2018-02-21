8a60325b8e711b55ac47d654c2882142b9aedb83
Tiziano <tiz.fortin@gmail.com>
2018-02-21 22:13:17 +0100

    new todolist repository

---
+++ src/main/java/ch/ricardo/project/repository/ToDoListRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.ricardo.project.repository;
+
+import ch.ricardo.project.entity.ToDoList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Tiziano (Titto) Fortin <tiz.fortin@gmail.com>
+ */
+public interface ToDoListRepository extends JpaRepository<ToDoList, Integer> { 
+    
+}

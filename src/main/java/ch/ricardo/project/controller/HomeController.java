295029678fc50866bf1e7e253df32d138fc243c7
Tiziano <tiz.fortin@gmail.com>
2018-02-24 16:48:23 +0100

    more comments and logs

--- src/main/java/ch/ricardo/project/controller/HomeController.java
+++ src/main/java/ch/ricardo/project/controller/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import ch.ricardo.project.service.TaskService;
 import ch.ricardo.project.service.ToDoListService;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -24,53 +23,83 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class HomeController {
 
     private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-    @Autowired
-    private TaskService taskService;
     
     @Autowired
     private ToDoListService toDoListService;
 
+    /**
+     * Get all the ToDoList in the database
+     * @return 
+     */
     @Timed
     @RequestMapping(path = "/todolist/getall", method = RequestMethod.GET)
     public List<ToDoList> getAllList() {
+        logger.info("Getting all ToDoList");
         List<ToDoList> results = this.toDoListService.getAllToDoList();
         if(logger.isDebugEnabled())
             logger.debug("result size is {}", results.size());
         return results;
     }
     
+    /**
+     * Get a ToDoList by ID
+     * @param id
+     * @return 
+     */
     @Timed
     @RequestMapping(path = "/todolist/{id}", method = RequestMethod.GET)
     public ToDoList getList(@PathVariable("id") String id) {
+        logger.info("Getting specific ToDoList with ID: {}", id);        
+        ToDoList result = this.toDoListService.getToDoList(Integer.parseInt(id));
         if(logger.isDebugEnabled())
-            logger.debug("Getting specific ToDoList with ID: {}", id);
-        return this.toDoListService.getToDoList(Integer.parseInt(id));
+            logger.debug("Result : {} ", result.toString());
+        return result;
     }
     
+    /**
+     * Update an existent ToDOList
+     * @param list
+     * @return 
+     */
     @Timed
     @RequestMapping(path = "/todolist", method = RequestMethod.PUT)
     public ToDoList updateList(@RequestBody ToDoList list) {
+        logger.info("Update the ToDoList: {}", list.toString());
         return this.toDoListService.updateList(list);
     }
     
+    /**
+     * Delete a ToDoList by ID
+     * @param id 
+     */
     @Timed
     @RequestMapping(path = "/todolist/{id}", method = RequestMethod.DELETE)
     public void deleteList(@PathVariable("id") String id) {
-        if(logger.isDebugEnabled())
-            logger.debug("Deleting specific ToDoList with ID: {}", id);
+        logger.info("Deleting specific ToDoList with ID: {}", id);
         this.toDoListService.removeToDoList(Integer.parseInt(id));
     }
     
+    /**
+     * create a new ToDoList
+     * @param list
+     * @return 
+     */
     @Timed
     @RequestMapping(path = "/todolist", method = RequestMethod.POST)
     public ToDoList createList(@RequestBody ToDoList list) {
+        logger.info("Create new ToDoList: {}", list.toString());
         return this.toDoListService.createToDoList(list);
     }
     
+    /**
+     * Get all the ToDoList by the user (owner)
+     * @param user
+     * @return 
+     */
    @Timed
    @RequestMapping(path = "/todolist/users/{user}", method = RequestMethod.GET)
    public List<ToDoList> getToDoListByUser(@PathVariable("user") String user) {
+       logger.info("Get ToDoLists for user: {}", user);
        return this.toDoListService.getListForUser(user);
    }
 
